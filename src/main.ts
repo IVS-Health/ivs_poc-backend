23556f1f2d1466451c7481c72af6d50ef7ba331f
Sourina Nandy <sourina.nandy@gmail.com>
2025-11-05 10:52:12 +0100

    updated main.ts for android port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5174',
+    origin: true,
     credentials: true,
   });
   app.useGlobalPipes(
@@ -13,8 +13,8 @@ async function bootstrap() {
       whitelist: true, // remove unknown fields automatically
       forbidNonWhitelisted: true, // throw error on unexpected fields
       transform: true, // converts payloads to DTO classes
-    }),
+    })
   );
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.APP_PORT || "0.0.0.0");
 }
 bootstrap();
